629bdecefcc3a9f34a216ec1ddb2f700c4a296de
japh <jenny.scherr@svenskaspel.se>
2020-05-03 12:03:12 +0200

    removing json in wrap parameter solves session permission, but not needed, it works fine to keep

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -18,4 +18,5 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
   config.include(Shoulda::Matchers::ActiveRecord, type: :model)
   config.include ResponseJSON
+  config.filter_gems_from_backtrace('capybara')
 end
